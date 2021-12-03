e35880b998d2aff48ca7466fc2244c49f79d2513
Ewoodss <70382659+Ewoodss@users.noreply.github.com>
2021-12-03 11:35:50 +0100

    show current location with a dude

--- app/src/main/java/com/example/gastles/MainActivity.kt
+++ app/src/main/java/com/example/gastles/MainActivity.kt
@@ -8,6 +8,8 @@ import com.example.gastles.databinding.ActivityMainBinding
 import org.osmdroid.config.Configuration.getInstance
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        getInstance().load(this, getSharedPreferences("osm_config", Context.MODE_PRIVATE));
+        getInstance().load(this, getSharedPreferences("osm_config", Context.MODE_PRIVATE))
 
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
 
@@ -27,11 +29,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun setupMaps()
     {
+
+
+
+
         binding.map.apply {
             setTileSource(TileSourceFactory.MAPNIK)
-            controller.setZoom(20.0)
-            val startPoint = GeoPoint(51.58567, 4.79428);
-            controller.setCenter(startPoint);
+
+            val locationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this@MainActivity), this)
+            locationOverlay.enableMyLocation()
+            overlays.add(locationOverlay)
+
+            setMultiTouchControls(true)
+
+            controller.apply {
+                setZoom(20.0)
+                val startPoint = GeoPoint(51.58567, 4.79428)
+                setCenter(startPoint)
+            }
+
         }
     }
 
